08354da6c87256782279db1104566b39cdf1687a
Naka <luismc9915@gmail.com>
2022-05-10 21:22:03 -0300

    Correção do resultado em divisão

---
+++ src/br/com/dio/calculadora/Calculadora.java
@@ -0,0 +1,42 @@
+package br.com.dio.calculadora;
+
+import java.util.Scanner;
+
+public class Calculadora {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+		int a, b;
+		
+		System.out.println("Digite o primeiro valor: ");
+		a = scan.nextInt();
+		System.out.println("Digite o segundo valor: ");
+		b = scan.nextInt();
+		
+		int soma = soma(a,b);
+		int subtracao = subtracao(a, b);
+		double divisao = divisao(a, b);
+		int multiplicacao = multiplicacao(a, b);
+		
+		System.out.println("Soma: " + soma);
+		System.out.println("Subtracao: "  + subtracao);
+		System.out.println("Divisao: " + divisao);
+		System.out.println("multiplicacao: " + multiplicacao);
+		
+	}
+	// Calculadora
+	public static int soma (int a, int b) {
+		return a + b;
+	}
+	public static int subtracao (int a, int b) {
+		return a - b;
+	}	
+	public static double divisao (double a, double b) {
+		return a / b;
+	}	
+	public static int multiplicacao (int a, int b) {
+		return a * b;
+	}	
+	
+}
